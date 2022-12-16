a59bedc94c6ea1afcbe942e5ac93b1485538a0a2
kauanfiolo-04 <kauanfiolo@hotmail.com>
2022-12-16 15:44:16 -0300

    estilização feita

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,33 +1,36 @@
 import { Button } from "./components/Button"
+import { useState } from "react"
 
 export function Calculator(){
-  return (
-    <div className="container h-[75%] min-h-[400px] w-[25%] min-w-[300px] bg-black my-10">
+  const [click,setClick]=useState(false)
 
-      <div className='flex justify-end text-white h-[20%] items-end mr-5 text-5xl'>
+  return (
+    <div className="container space-y-[25%] h-[75%] min-h-[400px] w-[25%] min-w-[300px] rounded-md  bg-black my-10">
+
+      <div className='flex justify-end items-end text-white h-[20%] mr-5 text-5xl '>
         <span>0</span>
       </div>
 
-      <div>
-        <Button name='AC'/>
-        <Button name='+/-' />
-        <Button name='%' />
-        <Button name='/' />
-        <Button name='7' />
-        <Button name='8' />
-        <Button name='9' />
-        <Button name='X' />
-        <Button name='4' />
-        <Button name='5' />
-        <Button name='6' />
-        <Button name='-' />
-        <Button name='1' />
-        <Button name='2' />
-        <Button name='3' />
-        <Button name='+' />
-        <Button name='0' />
-        <Button name=',' />
-        <Button name='=' />
+      <div className='w-[100%] h-[60%] grid grid-cols-4' >
+        <Button name='AC' type="idk"/>
+        <Button name='+/-' type="idk"/>
+        <Button name='%'  type="idk"/>
+        <Button name='/'  type="op"/>
+        <Button name='7'  />
+        <Button name='8'  />
+        <Button name='9'  />
+        <Button name='x'  type="op"/>
+        <Button name='4'  />
+        <Button name='5'  />
+        <Button name='6'  />
+        <Button name='-'  type="op"/>
+        <Button name='1'  />
+        <Button name='2'  />
+        <Button name='3'  />
+        <Button name='+'  type="op"/>
+        <Button name='0'  />
+        <Button name=','  />
+        <Button name='='  type="op"/>
       </div>
 
     </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,22 @@
-export  function Button(props){
+
+
+export  function Button({ name, type='num'}){
+  const number='flex text-white w-[80%] h-[95%] justify-center items-center rounded-full  text-2xl cursor-pointer p-2  bg-[#343434] self-center justify-self-center'
+
+  const operators='flex text-white w-[80%] h-[95%] justify-center items-center rounded-full  text-2xl cursor-pointer p-2  bg-[#FF9F0A] self-center justify-self-center'
+  
+  const zero='flex text-white col-span-2 w-[90%] h-[95%] justify-center items-center rounded-full text-2xl cursor-pointer p-2  bg-[#343434] self-center justify-self-center'
+  
+  const IDK='flex text-black w-[80%] h-[95%] justify-center items-center rounded-full text-2xl cursor-pointer p-2  bg-[#A5A5A5] self-center justify-self-center'
+
+  const setType=(t)=>{
+    if(name==='0'){return zero} if(t==='idk'){return IDK}
+    if(t==='num'){return number} if(t==='op'){return operators}
+  }
+
   return(
     <button 
-      className='mx-1 w-[60%] text-white border border-white'
-    >{props.name}</button>
+      className={setType(type)} 
+    >{name}</button>
   )
 }
