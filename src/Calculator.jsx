cee8b17f38f2865d20405deff10ea89f75bda357
kauanfiolo-04 <kauanfiolo@hotmail.com>
2022-12-21 11:27:32 -0300

    finalizado

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,36 +1,66 @@
-import { Button } from "./components/Button"
+/* eslint-disable no-eval */
 import { useState } from "react"
+import { Button } from "./components/Button"
 
 export function Calculator(){
+  const[result, setResult]=useState('')
+
+  const handleChange=(e)=>{
+    console.log(e)
+    setResult(result.concat(e.target.innerText))
+  }
+
+  const handleClear=()=>{
+    setResult('')
+  }
+
+  const handlePercent=()=>{
+    setResult((result/100).toString())
+  }
+
+  const handleSLA=()=>{
+    setResult((result*-1).toString())
+  }
+
+  const handleEvaluate=()=>{
+    try {
+      setResult(eval(result).toString())
+    } catch (error) {
+      setResult('Error')
+    }
+  }
+
+
   
 
   return (
-    <div className="container space-y-[25%] h-[75%] min-h-[400px] w-[25%] min-w-[300px] rounded-md  bg-black my-10">
+    <div className="container space-y-[25%] md:h-[70%] h-[600px] md:w-[20%] w-[300px]  rounded-md  bg-black my-10
+    ">
 
-      <div className='flex justify-end items-end text-white h-[20%] mr-5 text-5xl '>
-        <span>0</span>
+      <div className='flex justify-end items-end text-white h-[20%] mr-5 text-5xl'>
+        <span>{result}</span>
       </div>
 
-      <div className='w-[100%] h-[60%] grid grid-cols-4' >
-        <Button name='AC' type="idk"/>
-        <Button name='+/-' type="idk"/>
-        <Button name='%'  type="idk"/>
-        <Button name='/'  type="op"/>
-        <Button name='7'  />
-        <Button name='8'  />
-        <Button name='9'  />
-        <Button name='x'  type="op"/>
-        <Button name='4'  />
-        <Button name='5'  />
-        <Button name='6'  />
-        <Button name='-'  type="op"/>
-        <Button name='1'  />
-        <Button name='2'  />
-        <Button name='3'  />
-        <Button name='+'  type="op"/>
-        <Button name='0'  />
-        <Button name=','  />
-        <Button name='='  type="op"/>
+      <div className='w-[100%] h-[60%] grid grid-cols-4 space-x-1'>
+        <Button value='AC'  type="idk" handleChange={handleClear}/>
+        <Button value='+/-' type="idk" handleChange={handleSLA}/>
+        <Button value='%'  type="idk" handleChange={handlePercent}/>
+        <Button value='/' type="op" handleChange={handleChange}/>
+        <Button value='7'  handleChange={handleChange}/>
+        <Button value='8'  handleChange={handleChange}/>
+        <Button value='9'  handleChange={handleChange}/>
+        <Button value='*'  type="op" handleChange={handleChange}/>
+        <Button value='4'  handleChange={handleChange}/>
+        <Button value='5'  handleChange={handleChange}/>
+        <Button value='6'  handleChange={handleChange}/>
+        <Button value='-'  type="op" handleChange={handleChange}/>
+        <Button value='1'  handleChange={handleChange}/>
+        <Button value='2'  handleChange={handleChange}/>
+        <Button value='3'  handleChange={handleChange}/>
+        <Button value='+'  type="op" handleChange={handleChange}/>
+        <Button value='0'  handleChange={handleChange}/>
+        <Button value=','  handleChange={handleChange}/>
+        <Button value='='  type="op" handleChange={handleEvaluate}/>
       </div>
 
     </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,22 +1,22 @@
 
+export  function Button({ value, type='num', handleChange }){
+  const number='flex text-white w-[65%] h-[90%] justify-center items-center rounded-full  text-2xl cursor-pointer p-2  bg-[#343434] self-center justify-self-center'
 
-export  function Button({ name, type='num'}){
-  const number='flex text-white w-[80%] h-[95%] justify-center items-center rounded-full  text-2xl cursor-pointer p-2  bg-[#343434] self-center justify-self-center'
-
-  const operators='flex text-white w-[80%] h-[95%] justify-center items-center rounded-full  text-2xl cursor-pointer p-2  bg-[#FF9F0A] self-center justify-self-center'
+  const operators='flex text-white w-[65%] h-[90%] justify-center items-center rounded-full  text-2xl cursor-pointer p-2  bg-[#FF9F0A] self-center justify-self-center'
   
-  const zero='flex text-white col-span-2 w-[90%] h-[95%] justify-center items-center rounded-full text-2xl cursor-pointer p-2  bg-[#343434] self-center justify-self-center'
+  const zero='flex text-white col-span-2 w-[80%] h-[90%] justify-center items-center rounded-full text-2xl cursor-pointer p-2  bg-[#343434] self-center justify-self-center'
   
-  const IDK='flex text-black w-[80%] h-[95%] justify-center items-center rounded-full text-2xl cursor-pointer p-2  bg-[#A5A5A5] self-center justify-self-center'
+  const IDK='flex text-black w-[65%] h-[90%] justify-center items-center rounded-full text-2xl cursor-pointer p-2  bg-[#A5A5A5] self-center justify-self-center'
 
   const setType=(t)=>{
-    if(name==='0'){return zero} if(t==='idk'){return IDK}
+    if(value==='0'){return zero} if(t==='idk'){return IDK}
     if(t==='num'){return number} if(t==='op'){return operators}
   }
 
+
   return(
     <button 
-      className={setType(type)} 
-    >{name}</button>
+      className={setType(type)} onClick={handleChange}
+    >{value}</button>
   )
 }
